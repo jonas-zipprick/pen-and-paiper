f96fe63aa10b2e1f908a473998422bf07871cef4
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-14 12:51:07 +0200

    fix: Exclude lib in ts build

--- roll20-plugin/src/ts/index.ts
+++ roll20-plugin/src/ts/index.ts
@@ -1,10 +1,27 @@
+import "@types/roll20-ts";
+
 interface State {
     Paiper: PaiperState;
 }
 
 class Paiper {
+    constructor() {
 
+    }
+
+    public listen(): void {
+        on("chat:message", this.handleInput.bind(this));
+    }
+
+    private handleInput(msg: Message): void {
+        console.log(JSON.stringify(msg));
+    }
 }
 
 interface PaiperState {
 }
+
+on("ready", () => {
+    const p = new Paiper();
+    p.listen();
+});
