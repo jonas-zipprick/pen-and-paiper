8c882ed6feaf6a6c39fc10b158b4c290472d243b
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-14 21:22:54 +0200

    Disconnect on failure

--- mistral-client/src/client.py
+++ mistral-client/src/client.py
@@ -23,12 +23,13 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            try:
+                await connection.send_text(message)
+            except WebSocketDisconnect:
+                self.disconnect(connection)
+
 manager = ConnectionManager()
 
 
