1ff2f9a30a80f55d82efbd4865a7cd1c7e62dd4f
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-14 15:24:57 +0200

    Don't listen on state

--- backend/src/ts/index.ts
+++ backend/src/ts/index.ts
@@ -11,12 +11,6 @@ const server = http.createServer(app);
 
 bindWss(server);
 
-app.use(express.json());
-app.post('/state', (req) => {
-    console.log(JSON.stringify(req.body));
-});
-
-
 server.listen(config.port, () => {
     console.log(`🚀 Backend server is running and listening on http://localhost:${config.port}`);
 });
