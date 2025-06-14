7ead22df5b41451709d28148a4fc7658f398a61c
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-14 14:09:40 +0200

    feat(roll20-plugin): chat and changed graphics

--- roll20-plugin/src/ts/index.ts
+++ roll20-plugin/src/ts/index.ts
@@ -1,12 +1,8 @@
 import {on, Message, log, Roll20Object, getAllObjs} from 'roll20-ts';
 
 class Paiper {
-    private objs?: Roll20Object[] = undefined;
-    constructor() {
-        this.objs = getAllObjs();
-    }
-
-    public listen(): void {
+    public start(): void {
+        getAllObjs();
         on("chat:message", this.chatMessage.bind(this));
         on("change:graphic", this.changeGraphic.bind(this));
     }
@@ -17,10 +13,11 @@ class Paiper {
 
     private changeGraphic(obj: Roll20Object): void {
         log(JSON.stringify(obj));
+        log(getAllObjs());
     }
 }
 
 on("ready", () => {
     const p = new Paiper();
-    p.listen();
+    p.start();
 });
