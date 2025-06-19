dbd08d20d91315dc74a26ac20ae84fc2644d9ee1
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-19 17:51:37 +0200

    fix: frontend WEBSOCKET_URL

--- frontend/src/components/Stream.tsx
+++ frontend/src/components/Stream.tsx
@@ -5,7 +5,7 @@ import { Section } from './Section';
 
 
 export const StreamerView = () => {
-    const WEBSOCKET_URL = "ws://localhost:8000/assistant/ws";
+    const WEBSOCKET_URL = "wss://67934-3000.2.codesphere.com/assistant/ws";
     const [readNext, setReadNext] = useState<string>('The ether remains silent...');
     const [relatedRules, setRelatedRules] = useState<string>('No specific lore applies here...');
     const [happenNext, setHappenNext] = useState<string>('The threads of fate are unclear...');
