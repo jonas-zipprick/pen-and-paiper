dec93a09540f460e2856045c6269489b1a3b59e5
Jonas Zipprick <jonas.zipprick@gmail.com>
2025-06-19 18:00:02 +0200

    Improve websocket URL

--- frontend/src/components/Stream.tsx
+++ frontend/src/components/Stream.tsx
@@ -5,7 +5,7 @@ import { Section } from './Section';
 
 
 export const StreamerView = () => {
-    const WEBSOCKET_URL = "wss://67934-3000.2.codesphere.com/assistant/ws";
+    const WEBSOCKET_URL = `wss://${location.host}/assistant/listen`;
     const [readNext, setReadNext] = useState<string>('The ether remains silent...');
     const [relatedRules, setRelatedRules] = useState<string>('No specific lore applies here...');
     const [happenNext, setHappenNext] = useState<string>('The threads of fate are unclear...');
